58e1c2ffd625abe0e93a0e6b213fa19ffa4982a2
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 22:27:14 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int price = 7;
+			int gold;
+			int cristal;
+			Console.WriteLine("введите количество золота (целое число)");
+			gold = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"кристалов куплено - {gold / price}, золота осталось - {gold % price}");
+
+		}
+	}
+}
