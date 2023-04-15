293e342a713ec477d32569c1025b0b09be187194
van879 <121977210+van879@users.noreply.github.com>
2023-04-15 12:20:55 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,14 +9,16 @@ namespace task_7
 			int priceOfCristal = 7;
 			int gold;
 			int cristals;
-			int remainsOfGold;
+			int desiredCristals;
 
 			Console.WriteLine("введите количество золота (целое число)");
 			gold = Convert.ToInt32(Console.ReadLine());
-			cristals = gold / priceOfCristal;
-			remainsOfGold = gold % priceOfCristal;
+			Console.WriteLine("сколько кристалов хотите купить (целое число)");
+			cristals = Convert.ToInt32(Console.ReadLine());
+			gold -= cristals * priceOfCristal;
+			
 
-			Console.WriteLine($"кристалов куплено - {cristals}, золота осталось - {remainsOfGold}");
+			Console.WriteLine($"кристалов куплено - {cristals}, золота осталось - {gold}");
 		}
 	}
 }
